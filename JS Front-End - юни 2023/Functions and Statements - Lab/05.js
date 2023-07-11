ec38a3afeaeb175cdca3652045f7548acc6a9148
aEnchev669 <aenchev669@abv.bg>
2023-07-11 21:57:01 +0300

    Create 05.js

---
+++ JS Front-End - юни 2023/Functions and Statements - Lab/05.js
@@ -0,0 +1,19 @@
+function solve(num1, num2, operator) {
+
+    switch (operator) {
+        case 'multiply':
+            console.log(num1 * num2);
+            break;
+        case 'divide':
+            console.log(num1 / num2);
+            break;
+        case 'add':
+            console.log(num1 + num2);
+            break;
+        case 'subtract':
+            console.log(num1 - num2);
+            break;
+    }
+}
+
+solve(5, 5, 'subtract')
