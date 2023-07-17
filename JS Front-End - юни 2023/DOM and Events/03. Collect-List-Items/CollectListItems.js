cf597c1d95bac7d28bbefdf71e80da8f573d8c46
aEnchev669 <aenchev669@abv.bg>
2023-07-17 15:08:24 +0300

    Update CollectListItems.js

--- JS Front-End - юни 2023/DOM and Events/03. Collect-List-Items/CollectListItems.js
+++ JS Front-End - юни 2023/DOM and Events/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    let items = document.getElementById('items');
+    let result = document.getElementById('result');
+
+    
+    result.value = items.textContent;
+    
 }
