4014276571adbaed137da08b13d8980411b73cbe
aEnchev669 <aenchev669@abv.bg>
2023-07-24 10:53:12 +0300

    Update app.js

--- JS Front-End - юни 2023/DOM and Events/04. List-Of-Items/app.js
+++ JS Front-End - юни 2023/DOM and Events/04. List-Of-Items/app.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+
+    let items = document.getElementById('items');
+    let result = document.getElementById('newItemText');
+
+    let li = document.createElement('li');
+
+    li.textContent = result.value;
+    result.value = ' ';
+    items.appendChild(li);
+
 }
