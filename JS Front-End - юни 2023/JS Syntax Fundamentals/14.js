e0df8339d4c26a5dfcd42a1234a7bbd46a516d4e
aEnchev669 <aenchev669@abv.bg>
2023-07-26 09:44:20 +0300

    Create 14.js

---
+++ JS Front-End - юни 2023/JS Syntax Fundamentals/14.js
@@ -0,0 +1,9 @@
+function substringg(array, start, end) {
+
+    let lenght = end - start;
+
+    console.log(array.substr(start, end));
+}
+
+substringg('ASentence', 1, 8);
+substringg('SkipWord', 4, 7);
