3b2ded44b85397d989889cdcec37fce1c44ee83f
aEnchev669 <aenchev669@abv.bg>
2023-07-26 09:44:11 +0300

    Update solution.js

--- JS Front-End - юни 2023/DOM and Events/08. Furniture/solution.js
+++ JS Front-End - юни 2023/DOM and Events/08. Furniture/solution.js
@@ -1,4 +1,51 @@
 function solve() {
 
-  //TODO...
+  const button = document.querySelector('#exercise button');
+  button.addEventListener('click', parseInput);
+}
+
+function parseInput() {
+  const input = JSON.parse(document.querySelector('#exercise textarea').value);
+  const tbody = document.querySelector('tbody');
+
+
+  input
+    .map((furniture) => {
+      let row = document.createElement('tr');
+
+      let imgTd = document.createElement('td');
+      let img = document.createElement('img');
+      img.src = furniture.img;
+      imgTd.append(img);
+
+      let productTd = document.createElement('td');
+      let productP = document.createElement('p');
+      productP.textContent = furniture.name;
+      productTd.appendChild(productP);
+
+      let priuceTd = document.createElement('td');
+      let price = document.createElement('p');
+      price.textContent = furniture.price;
+      priuceTd.appendChild(price);
+
+      let decFacTd = document.createElement('td');
+      let decFac = document.createElement('p');
+      decFac.textContent = furniture.decFactor;
+      decFacTd.appendChild(decFac);
+
+      let inputTd = document.createElement('td');
+      let checkBox = document.createElement('input');
+      checkBox.type = 'checkbox';
+      inputTd.appendChild(checkBox);
+
+      row.appendChild(imgTd);
+      row.appendChild(productTd);
+      row.appendChild(priuceTd);
+      row.appendChild(decFacTd);
+      row.appendChild(inputTd);
+
+      return row;
+    })
+    .forEach((row) => tbody.appendChild(row));
+
 }
