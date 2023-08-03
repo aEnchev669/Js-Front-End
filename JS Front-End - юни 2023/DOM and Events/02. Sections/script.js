1786370294d1f340a15c3ca972dcbd3c2aea298e
aEnchev669 <aenchev669@abv.bg>
2023-08-03 12:41:14 +0300

    Update script.js

--- JS Front-End - юни 2023/DOM and Events/02. Sections/script.js
+++ JS Front-End - юни 2023/DOM and Events/02. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+
+   words.forEach(word => {
+
+
+      let div = document.createElement('div');
+      let p1 = document.createElement('p');
+      p1.textContent = word;
+      p1.style.display = 'none';
+      div.appendChild(p1);
+      const element = document.getElementById('content');
+      element.appendChild(div);
+
+      div.addEventListener('click', () => {
+         p1.style.display = 'block';
+      })
+
+
+   });
 }
